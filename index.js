881cba9b2e324f10ba5d9ba315c6e04064000551
Massimo-gif <77878966+Massimo-gif@users.noreply.github.com>
2021-01-25 18:48:42 +0100

    text

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function get (){
-	//滚动条距离顶部的距离
-	var topverb1 = document.documentElement.scrollTop;
-        //body文本内容的高度,也可以直接设置文本的高度但不能加 px
-	var topverb2 = document.body.scrollHeight= 300;
-  //var topverb2 = document.body.scrollHeight="300";
-	if(topverb1>=topverb2/3){
-		document.getElementById("gethideen").style.display="inherit";
-	}else{
-		document.getElementById("gethideen").style.display="none";
-	}
-}
 
+var nomediutente = window.prompt ("Per accedere al web inserisci il tuo nome(o un nome qualsiasi)");
+document.getElementById("Titolo").innerHTML = "Benvenuto '" + nomediutente + "' al mio primo web";
